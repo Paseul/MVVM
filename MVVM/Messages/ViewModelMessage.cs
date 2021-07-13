da2a0f31b17041af4c97bdd3041c6795b3b1fab4
‘Paseul <machianb@naver.com>
2021-07-13 17:20:32 +0900

    update messages & brushes

---
+++ MVVM/Messages/ViewModelMessage.cs
@@ -0,0 +1,18 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Messages
+{
+    class ViewModelMessage : MessageBase
+    {
+        public string d1 { get; set; }
+        public string d2 { get; set; }
+        public string d3 { get; set; }
+        public string d4 { get; set; }
+        public byte ba { get; set; }
+    }
+}
---
+++ MVVM/Model/Brushes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Model
+{
+    public class Brushes
+    {
+        private string brushName;
+
+        public string BrushName
+        {
+            get { return brushName; }
+            set { brushName = value; }
+        }
+    }
+}
